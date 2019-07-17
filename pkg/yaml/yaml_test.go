b8f0b5dc10b084b5ebdfd7481eafe91601938ed4
Stacie Graves <stacie.graves@gmail.com>
2019-07-17 09:44:08 -0600

    added additional test case

--- pkg/yaml/yaml_test.go
+++ pkg/yaml/yaml_test.go
@@ -130,5 +130,6 @@ func TestResolverCollections(t *testing.T) {
 
 	assert.Equal(t, "http://localhost:8080", resolved["baseUrl"])
 	assert.Equal(t, []interface{}{map[string]interface{}{"multi_one_one":"one-one", "multi_one_two":"one-two"}, map[string]interface{}{"multi_two_one":"two-one", "multi_two_two":"two-two"}}, resolved["multiValCol"])
+	assert.Equal(t, []interface{}{map[string]interface{}{"multi_one_one":"one-one", "multi_one_two":"one-two"}, map[string]interface{}{"multi_two_one":"two-one", "multi_two_two":"two-two"}}, resolved["multiValColAgain"])
 	assert.Equal(t, []interface{}{"one", "two", "three"}, resolved["col"])
 }
