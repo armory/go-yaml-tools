cab75bf9201e67558f4119761dbe986e261cdcd0
Nicolas Cohen <ncknt@users.noreply.github.com>
2020-01-29 17:56:02 -0800

    feat(tls/client): client side support for mTLS with custom CA (#23)
    * feat(tls/client): client side support for mTLS with custom CA
    
    * feat(tls/client): client side support for mTLS with custom CA

--- pkg/tls/tls_test.go
+++ pkg/tls/tls_test.go
@@ -1,4 +1,4 @@
-package server
+package tls
 
 import (
 	"fmt"
@@ -36,14 +36,7 @@ func TestEncryptedKey(t *testing.T) {
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			s := Server{
-				config: &ServerConfig{
-					Ssl: Ssl{
-						KeyPassword: c.password,
-					},
-				},
-			}
-			p, err := s.getKeyPassword()
+			p, err := getKeyPassword(c.password)
 			if assert.Equal(t, c.errExpected, err != nil) {
 				assert.Equal(t, c.expected, p)
 			}
@@ -52,13 +45,13 @@ func TestEncryptedKey(t *testing.T) {
 }
 
 func TestFileReadable(t *testing.T) {
-	err := checkFileExists(fmt.Sprintf("encrypted:noop!asdf"))
+	err := CheckFileExists(fmt.Sprintf("encrypted:noop!asdf"))
 	if !assert.NotNil(t, err) {
 		return
 	}
 	assert.Equal(t, "no file referenced, use encryptedFile", err.Error())
 
-	err = checkFileExists(fmt.Sprintf("encryptedFile:noop!asdf"))
+	err = CheckFileExists(fmt.Sprintf("encryptedFile:noop!asdf"))
 	if !assert.Nil(t, err) {
 		return
 	}
@@ -71,9 +64,9 @@ func TestFileReadable(t *testing.T) {
 	defer tmpfile.Close()
 
 	// File should be readable
-	assert.Nil(t, checkFileExists(tmpfile.Name()))
+	assert.Nil(t, CheckFileExists(tmpfile.Name()))
 
 	// Remove the file
 	os.Remove(tmpfile.Name())
-	assert.NotNil(t, checkFileExists(tmpfile.Name()))
+	assert.NotNil(t, CheckFileExists(tmpfile.Name()))
 }
